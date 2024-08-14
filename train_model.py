b64c202e2450a40a244c1513d69d4ad244246d33
Rob Lau <rlau21@wgu.edu>
2024-08-14 20:11:41 +0000

    train model updates

--- train_model.py
+++ train_model.py
@@ -13,7 +13,7 @@ from ml.model import (
     train_model,
 )
 # TODO: load the cencus.csv data
-project_path = "" #leaving blank
+project_path = "./" #leaving blank
 data_path = os.path.join(project_path, "data", "census.csv")
 print(data_path)
 data = pd.read_csv(data_path) # your code here, using pandas read function
